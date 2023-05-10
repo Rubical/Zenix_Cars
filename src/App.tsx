a2189deacb3129f9550980427d445ce562856160
Rubical <zhpiv96@mail.ru>
2023-05-10 21:04:13 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+    <img src='https://cdn.imagin.studio/getImage?&make=mercedes&modelFamily=s&customer=img' alt='car'/>
+      <img src='https://cdn.imagin.studio/getImage?&make=bmw&modelFamily=7&customer=img' alt='car'/>
+        <img src='https://cdn.imagin.studio/getImage?&make=audi&modelFamily=a8&customer=img' alt='car'/>
+
+      </>
   );
 }
 
